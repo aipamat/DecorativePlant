0e0e2d8b089dd871d5ce657586e7f21896e3d13c
Arif Rachmat Darmawan <arifrd200211@gmail.com>
2023-08-14 20:15:21 +0700

    Initial commit

---
+++ app/src/main/java/com/arif/decorativeplant/activities/SplashScreenActivity.java
@@ -0,0 +1,31 @@
+package com.arif.decorativeplant.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.arif.decorativeplant.R;
+import com.arif.decorativeplant.onboarding.OnboardingActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    private int waktu_loading = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                //setelah loading maka akan langsung berpindah ke home activity
+                Intent home = new Intent(SplashScreenActivity.this, OnboardingActivity.class);
+                startActivity(home);
+                finish();
+
+            }
+        }, waktu_loading);
+    }
+}
