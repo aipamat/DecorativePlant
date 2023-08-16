3cd48ef0280eb540f784cfd1b8cdc9ba0c3365da
Arif Rachmat Darmawan <arifrd200211@gmail.com>
2023-08-16 17:18:08 +0700

    Final

--- app/src/main/java/com/arif/decorativeplant/activities/DetailActivity.java
+++ app/src/main/java/com/arif/decorativeplant/activities/DetailActivity.java
@@ -2,18 +2,9 @@ package com.arif.decorativeplant.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
-import android.app.Activity;
-import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.arif.decorativeplant.R;
 import com.arif.decorativeplant.model.ModelMain;
 import com.bumptech.glide.Glide;
@@ -37,18 +28,6 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-
-        // Set status bar menjadi transparan pada Android Marshmallow (API level 23) ke atas.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
-                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-        }
-
-        // Set status bar transparan pada Android Lollipop (API level 21) ke atas.
-        if (Build.VERSION.SDK_INT >= 21) {
-            setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false);
-            getWindow().setStatusBarColor(Color.TRANSPARENT);
-        }
 
         // Inisialisasi elemen-elemen UI.
         toolbar = findViewById(R.id.toolbar);
@@ -77,29 +56,6 @@ public class DetailActivity extends AppCompatActivity {
             tvManfaatTanaman.setText(strManfaatTanaman);
         }
 
-    }
-
-    // Fungsi ini akan dipanggil ketika salah satu item di action bar diklik.
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            // Mengakhiri aktivitas saat tombol back di action bar ditekan.
-            finish();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    // Fungsi ini digunakan untuk mengatur beberapa flag pada window.
-    public static void setWindowFlag(Activity activity, final int bits, boolean on) {
-        Window window = activity.getWindow();
-        WindowManager.LayoutParams layoutParams = window.getAttributes();
-        if (on) {
-            layoutParams.flags |= bits; // Mengatur bit flag jika on (true).
-        } else {
-            layoutParams.flags &= ~bits; // Menghapus bit flag jika off (false).
-        }
-        window.setAttributes(layoutParams);
     }
 
 }
--- app/src/main/java/com/arif/decorativeplant/activities/MainActivity.java
+++ app/src/main/java/com/arif/decorativeplant/activities/MainActivity.java
@@ -1,30 +1,20 @@
 package com.arif.decorativeplant.activities;
 
-import static com.arif.decorativeplant.activities.DetailActivity.setWindowFlag;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.app.Activity;
 import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
 import android.widget.SearchView;
 import android.widget.Toast;
-
 import com.arif.decorativeplant.R;
 import com.arif.decorativeplant.adapter.MainAdapter;
 import com.arif.decorativeplant.model.ModelMain;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
@@ -46,18 +36,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Set status bar menjadi transparan pada Android Marshmallow (API level 23) ke atas.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
-                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-        }
-
-        // Set status bar transparan pada Android Lollipop (API level 21) ke atas.
-        if (Build.VERSION.SDK_INT >= 21) {
-            setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false);
-            getWindow().setStatusBarColor(Color.TRANSPARENT);
-        }
 
         // Inisialisasi elemen-elemen UI.
         rvListTanaman = findViewById(R.id.rvListTanaman);
@@ -127,15 +105,4 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    // Fungsi ini digunakan untuk mengatur beberapa flag pada window.
-    public static void setWindowFlag(Activity activity, final int bits, boolean on) {
-        Window window = activity.getWindow();
-        WindowManager.LayoutParams layoutParams = window.getAttributes();
-        if (on) {
-            layoutParams.flags |= bits; // Mengatur bit flag jika on (true).
-        } else {
-            layoutParams.flags &= ~bits; // Menghapus bit flag jika off (false).
-        }
-        window.setAttributes(layoutParams);
-    }
 }
